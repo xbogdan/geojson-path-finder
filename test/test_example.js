544797a97649d108fc999d4a7c4ebe73b1502005
Bogdan BOAMFA <bboamfa.externe@solocal.com>
2021-01-13 19:01:42 +0200

    Remove unecessary code.

--- test/test_example.js
+++ test/test_example.js
@@ -1,9 +1,9 @@
-var PathFinder = require('../index.js'),
-    geojson = require('./network3.json');
-var point = require('turf-point');
-var distance = require('@turf/distance').default;
+const PathFinder = require('../index.js'),
+      geojson = require('./network3.json'),
+      point = require('turf-point'),
+      distance = require('@turf/distance').default;
 
-var highwaySpeeds = {
+const highwaySpeeds = {
     motorway: 110,
     trunk: 90,
     primary: 80,
@@ -16,34 +16,43 @@ var highwaySpeeds = {
     living_street: 20
 };
 
-var unknowns = {};
+const unknowns = {};
 
 function weightFn(a, b, props) {
-    var d = distance(point(a), point(b)) * 1000,
+    let d = distance(point(a), point(b)) * 1000,
         factor = 0.9,
         type = props.highway,
         forwardSpeed,
         backwardSpeed;
+
     if (props.maxspeed) {
         forwardSpeed = backwardSpeed = Number(props.maxspeed);
     } else {
-        var linkIndex = type.indexOf('_link');
+        let linkIndex = type.indexOf('_link');
+
         if (linkIndex >= 0) {
             type = type.substring(0, linkIndex);
             factor *= 0.7;
         }
+    
         forwardSpeed = backwardSpeed = highwaySpeeds[type] * factor;
+    
         if (!forwardSpeed) {
             unknowns[type] = true;
         }
     }
+    
     if (props.oneway && props.oneway !== 'no' || props.junction && props.junction === 'roundabout') {
         backwardSpeed = null;
     }
+
     return {
         forward: forwardSpeed && (d / (forwardSpeed / 3.6)),
         backward: backwardSpeed && (d / (backwardSpeed / 3.6)),
     };
 }
-var pathFinder = new PathFinder(geojson);
-console.log(pathFinder.findPointsAround(point([11.995450000000002, 57.71257000000001]), 5));
+
+const pathFinder = new PathFinder(geojson);
+const points = pathFinder.findPointsAround(point([11.995450000000002, 57.71257000000001]), 5);
+
+console.log(points);
